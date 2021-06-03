caf5b7ff63bc28080a70b984cf5e6efb07a7098b
a <narendrak.saran@gmail.com>
2021-06-04 01:11:52 +0200

    Move rules read logic to a static method
    Also add empty method to start http sync

--- blocker.py
+++ blocker.py
@@ -8,17 +8,25 @@ ACTION_ALLOW = "ALLOW"
 ACTION_BLOCK = "BLOCK"
 
 
+def start_http_sync():
+   pass
+
+
 class Blocker:
     def __init__(self):
-        f = open(CONFIG_FILE, "r")
-        config = json.load(f)
-        f.close()
-        self.rules = config['urls'] if config['urls'] else []
+        self.rules = Blocker.read_rules()
         self.log_rules()
-
+        start_http_sync()
     def request(self, flow):
         if next(filter(lambda url: url in flow.request.pretty_url, self.rules), None):
             flow.response = http.HTTPResponse.make(404, b"How about studying", {"Content-Type": "text/html"})
+
+    @staticmethod
+    def read_rules():
+        f = open(CONFIG_FILE, "r")
+        config = json.load(f)
+        f.close()
+        return config['urls'] if config['urls'] else []
 
     def log_rules(self):
         ctx.log.info("Blocked URLs are: %s" % str(self.rules))
