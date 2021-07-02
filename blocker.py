402a06d031f214e3c239241764052211efefae6b
mrnakumar <nknkchoudhary@gmail.com>
2021-07-02 22:44:32 +0200

    Add test for DB read write of browsed urls

--- blocker.py
+++ blocker.py
@@ -1,4 +1,3 @@
-import http.client
 import os
 import sys
 import threading
@@ -6,7 +5,6 @@ import time
 import urllib.parse
 import unittest
 
-import parameterized
 from mitmproxy import http as mitm_http
 from mitmproxy import ctx
 import getpass
@@ -40,15 +38,15 @@ class Blocker:
         self.db = Database()
         self.rules = self.db.read_blocked()
         self.should_log = should_log
-        if self.should_log:
-            self.log_blocked()
+        self.log_blocked()
 
     def request(self, flow):
         if next(filter(lambda url: url in flow.request.pretty_url, self.rules), None):
             flow.response = mitm_http.HTTPResponse.make(404, b"How about studying", {"Content-Type": "text/html"})
 
     def log_blocked(self):
-        ctx.log.info("Blocked host names are: %s" % str(self.rules))
+        if self.should_log:
+            ctx.log.info("Blocked host names are: %s" % str(self.rules))
 
 
 class Database:
@@ -58,14 +56,32 @@ class Database:
 
     def read_blocked(self):
         host_names = []
-        for host in self.db.execute(query_select_blocked):
+        cursor = self.db.cursor()
+        for host in cursor.execute(query_select_blocked):
             host_names.append(host)
+        cursor.close()
         return host_names
 
     def write_browsed(self, browsed_urls):
-        to_insert = list(map(to_record, browsed_urls))
-        self.db.executemany(query_insert_many, to_insert)
+        to_insert = map(lambda r: (r.user, r.host, r.url, r.accessed_on),
+                        filter(lambda x: x is not None, list(map(to_record, browsed_urls))))
+        cursor = self.db.cursor()
+        cursor.executemany(query_insert_many, to_insert)
         self.db.commit()
+        cursor.close()
+
+    # For testing
+    def read_browsed(self):
+        cursor = self.db.cursor()
+        result = []
+        for row in cursor.execute(f'SELECT distinct url_host from {TABLE_BROWSED}'):
+            result.append(row)
+        cursor.close()
+        return result
+
+    def close(self):
+        self.db.commit()
+        self.db.close()
 
     def create_tables(self):
         c = self.db.cursor()
@@ -92,19 +108,7 @@ def to_record(url):
     return BlockedRecord(user, host, url, time.time())
 
 
-def get_host_name(url):
-    def close(self):
-        self.browsed.commit()
-        self.blocked.commit()
-        self.browsed.close()
-        self.blocked.close()
-
-
 # tests
-def create_db():
-    blocked_db = sqlite3.connect(db_name)
-
-
 class TestToRecord(unittest.TestCase):
     def test_to_record(self):
         cases = [
@@ -123,6 +127,14 @@ class TestToRecord(unittest.TestCase):
             else:
                 self.assertEqual(result.host, host)
                 self.assertEqual(result.url, url)
+
+
+class TestDBReadWrite(unittest.TestCase):
+    def test_db_read_write(self):
+        db = Database()
+        db.write_browsed(["https://youtube.com", "https://twitter.com"])
+        result = db.read_browsed()
+        self.assertListEqual(list(map(lambda r: r[0], result)), ["youtube.com", "twitter.com"])
 
 
 # Export plugin
