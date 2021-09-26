39df55ffb2c94aa546ece315a7c78fc9fd219fee
narendra kumar <nknkchoudhary@gmail.com>
2021-09-26 20:27:53 +0200

    Donot include blocked URLs from browsed URLs

--- admin_service.py
+++ admin_service.py
@@ -1,10 +1,11 @@
 from flask import render_template
 
 class DatabaseService:
-
     def render_admin_page(self, db):
-        hostnames = self.get_browsed_hostnames(db)
-        return render_template('admin.html', hostnames = hostnames)
+        browsed = self.get_browsed_hostnames(db)
+        blocked = db.read_blocked()
+        not_blocked  = filter(lambda hostname: hostname not in blocked, browsed)
+        return render_template('admin.html', hostnames = not_blocked)
 
     def block_hostname(self, db, hostname):
         db.write_blocked([hostname,])
