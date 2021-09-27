3c82b66c4faba97127ebbd8e2417ba0c3d9704e7
narendra kumar <nknkchoudhary@gmail.com>
2021-09-27 23:44:28 +0200

    WIP: show hide browsed urls upon click on +/-- button

--- static/admin.js
+++ static/admin.js
@@ -2,3 +2,18 @@ function submit_form(form_id) {
 	var form = document.getElementById(form_id);
 	form.submit();
 }
+
+function showOrHideUrls(span_id, browsed_urls_panel_id) {
+ var hideClass = 'display-not';
+ var span = document.getElementById(span_id);
+ if(span !== null && span !== undefined) {
+	 var value = span.innerHTML;
+	 if(value === '+') {
+		document.getElementById(browsed_urls_panel_id).classList.remove(hideClass);
+	  span.innerHTML = '--';
+	 } else {
+		document.getElementById(browsed_urls_panel_id).classList.add(hideClass);
+	  span.innerHTML = '+';
+	 }
+ }
+}
